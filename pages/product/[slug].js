2d71d1d7e0eeb3a617c6378292b074bc8f47890f
Evan-nbl <evannbl.pro@gmail.com>
2024-11-21 14:46:09 +0100

    [ADD] - Fetch product details from API and display on product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,34 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
  
 export default function Page() {
 
-  // requête api sur api/product?id=
+  const router = useRouter();
+  const [product, setProduct] = useState({});
   
+  // requête api sur api/product?id=
+  const fetchProduct = async () => {
+    try {
+      const response = await fetch(`/api/product?id=${router.query.slug}`);
+      const data = await response.json();
+      if (response.ok) {
+        setProduct(data);
+      }
+    }
+    catch (error) {
+      console.error('Erreur lors de la récupération du produit:', error);
+    }
+  }
 
-  const router = useRouter()
-  return <p>Post: {router.query.slug}</p>
+  useEffect(() => {
+    fetchProduct();
+  }, [router.query.slug]);
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price} {product.currency}</p>
+    </div>
+  );
 }
